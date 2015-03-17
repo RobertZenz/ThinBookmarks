4a5c5a8306c628ef4918e3c80c7d0768bc0ed1a7
Robert Zenz <Bobby@bonsaimind.org>
2015-03-17 21:10:07 +0100

    Removed node about the fork, as this file has changed quite much.

--- src/content/javascript/ThinBookmarks.js
+++ src/content/javascript/ThinBookmarks.js
@@ -2,9 +2,6 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * This is a fork/based on Small Tabs, written by ロシア,
- * https://addons.mozilla.org/en-US/firefox/addon/small-tabs/
  */
 
 "use strict";
